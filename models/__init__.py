4e4ccf94727be736d3fad35f93550df38875b061
Damian Larrascq <damian.larrascq@gmail.com>
2026-06-12 02:55:33 -0300

    fix: imports

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from models.entidad_base import EntidadBase
 from models.libro import Libro
 from .recurso import Recurso, Libro, AudioLibro
 from .recurso_estados import EstadoRecurso, EstadoDisponible, EstadoPrestado
-from
+from .usuario import Usuario, Alumno, Profesor
+from .prestamo import Prestamo
 
-__all__ = ["EntidadBase", "Libro"]
+__all__ = ["EntidadBase", "Libro", "Recurso", "AudioLibro", "EstadoRecurso", "EstadoPrestado", "EstadoDisponible", "Usuario", "Alumno", "Profesor", "Prestamo"]
